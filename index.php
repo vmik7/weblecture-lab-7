5b47d9c1978d39f15362f6dd0495ad2b6504b574
vmik7 <vmik7@yandex.ru>
2021-04-14 12:13:47 +0300

    Базовая разметка и скрипты

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab7 - Mikerin</title>
+
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body class="page">
+    <?php include 'php/header.php' ?>
+    <main class="page__content content">
+        
+    </main>
+</body>
+</html>
